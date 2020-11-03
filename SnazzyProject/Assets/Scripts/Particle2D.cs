bb4998b2c8ac3d7d2b6be12129ae8b8c72fb9d54
cedric.brownewell <cedric.brownewell@mymail.champlain.edu>
2020-11-03 10:36:33 -0500

    Set up project and basic Particle2D script

---
+++ SnazzyProject/Assets/Scripts/Particle2D.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle2D : MonoBehaviour
+{
+   public float mMass;
+   public float mDamp;
+   public Vector2 mVel;
+   public Vector2 mAcc;
+
+   Vector2 accumlatedForces;
+}
