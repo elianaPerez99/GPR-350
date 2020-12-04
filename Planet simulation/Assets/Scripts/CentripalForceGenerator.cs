82ec6c02797c2d0b724bee5448af9b8be9dc777f
cedric.brownewell <cedric.brownewell@mymail.champlain.edu>
2020-12-03 22:24:04 -0500

    Planet now moves in a circle around sun.  IT goes out of bounds, but the angle should be set up correctly now

---
+++ Planet simulation/Assets/Scripts/CentripalForceGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CentripalForceGenerator
+{
+
+
+   public static Vector3 CalcCentripalAcc(Vector3 vel, float radius, float mass)
+   {
+
+      float temp = Vector3.Dot(vel, vel) / radius * mass;
+
+      // velocity = sqrt(G * earthMass / r)
+
+
+
+      return new Vector3();
+   }
+
+
+
+
+
+
+
+
+}
--- Planet simulation/Assets/Scripts/UniversalGravitationGenerator.cs
+++ Planet simulation/Assets/Scripts/UniversalGravitationGenerator.cs
@@ -6,50 +6,62 @@ using UnityEngine;
 
 public static class UniversalGravitationGenerator
 {
-    public static float G = (float)(6.67408 * Math.Pow(10, -11));
+   public static float G = (float)(6.67408 * Math.Pow(10, -11));
 
-    public static Vector3 CalculateForce(PlanetaryObject bigger, PlanetaryObject smaller)
-    {
-        float magnitude = (float)((G * bigger.mMass * smaller.mMass) / Math.Pow(smaller.transform.position.magnitude, 2));
-        float angle = (float)Math.Atan(magnitude);
-        
-        
-        float xMagnitude = (float) Math.Cos(angle)*magnitude;
-        if (smaller.transform.position.x < 0)
-        {
-            xMagnitude *= -1;
-        }
-        float zMagnitude = (float)Math.Cos(angle)*magnitude;
-        if (smaller.transform.position.z < 0)
-        {
-            zMagnitude *= -1;
-        }
-        return new Vector3(xMagnitude, 0.0f, 0.0f);
-    }
+   public static Vector3 CalculateForce(PlanetaryObject bigger, PlanetaryObject smaller)
+   {
+      //float magnitude = (float)((G * bigger.mMass * smaller.mMass) / Math.Pow(smaller.transform.position.magnitude, 2));
 
-    //get acceleration from force
-    private static Vector3 GetAcc(float mass, Vector3 force)
-    {
-        return force / mass;
-    }
+      //Vector3 planetDiff = bigger.transform.position - smaller.transform.position
+      //float magnitude = G * ((bigger.mMass * smaller.mMass) / Vector3.Dot(planetDiff, planetDiff));
 
-    //get current velocity based on current acceleration
-    private static Vector3 GetVel(Vector3 acc)
-    {
-        return acc / Time.deltaTime;
-    }
+      //Debug.Log(Vector3.Dot(planetDiff, planetDiff));
 
-    //assuming acceleration and velocity have already been updated based on the force
-    private static void UpdatePosition(PlanetaryObject planet)
-    {
-        planet.transform.position += planet.mCurrentVel * Time.deltaTime + planet.mCurrentAcc * (float)Math.Pow(Time.deltaTime, 2);
-    }
+      float magnitude = (float)((G * bigger.mMass * smaller.mMass) / Math.Pow(smaller.transform.position.magnitude, 2));
+      float angle = (float)Math.Atan(magnitude);
+      angle *= Mathf.Rad2Deg;
 
-    public static void Integrate(PlanetaryObject planet)
-    {
-        UpdatePosition(planet);
-        planet.mCurrentAcc = GetAcc(planet.mMass, planet.mCurrentForces);
-        planet.mCurrentVel = GetVel(planet.mCurrentAcc);
-        planet.mCurrentForces = new Vector3(0, 0, 0);
-    }
+
+      float xMagnitude = (float)Math.Cos(angle) * magnitude;
+      if (smaller.transform.position.x < 0)
+      {
+         xMagnitude *= -1;
+      }
+
+      float zMagnitude = (float)Math.Cos(angle) * magnitude;
+      if (smaller.transform.position.z < 0)
+      {
+         zMagnitude *= -1;
+      }
+
+      return new Vector3(xMagnitude, 0.0f, zMagnitude);
+   }
+
+   //get acceleration from force
+   private static Vector3 GetAcc(float mass, Vector3 force)
+   {
+      return force / mass;
+   }
+
+   //get current velocity based on current acceleration
+   private static Vector3 GetVel(Vector3 acc)
+   {
+      return acc * Time.deltaTime;//Cedric Code
+      //return acc / Time.deltaTime;
+   }
+
+   //assuming acceleration and velocity have already been updated based on the force
+   private static void UpdatePosition(PlanetaryObject planet)
+   {
+      planet.transform.position += planet.mCurrentVel * Time.deltaTime; //Cedric Code
+      //planet.transform.position += planet.mCurrentVel * Time.deltaTime + planet.mCurrentAcc * (float)Math.Pow(Time.deltaTime, 2);
+   }
+
+   public static void Integrate(PlanetaryObject planet)
+   {
+      UpdatePosition(planet);
+      planet.mCurrentAcc = GetAcc(planet.mMass, planet.mCurrentForces);
+      planet.mCurrentVel = GetVel(planet.mCurrentAcc);
+      planet.mCurrentForces = new Vector3(0, 0, 0);
+   }
 }
